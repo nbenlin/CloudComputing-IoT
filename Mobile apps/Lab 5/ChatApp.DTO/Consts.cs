07d0cfbf6c4676bb99c9390b22843d014e9d10eb
Nihat <nihatbenli.nb@gmail.com>
2021-05-02 19:48:46 +0200

    Add files via upload

---
+++ Mobile apps/Lab 5/ChatApp.DTO/Consts.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChatApp.DTO
+{
+    public class Consts
+    {
+        public const string RECEIVE_MESSAGE = "ReceiveMessage";
+
+        public const string SEND_MESSAGE = "SendMessage";
+
+        public const string USER_JOINED = "UserJoined";
+        public const string REGISTER_USER = "RegisterUser";
+
+    }
+}
---
+++ Mobile apps/Lab 5/ChatApp.DTO/Models/UserChatMessage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChatApp.DTO.Models
+{
+    public class UserChatMessage
+    {
+        public string Username { get; set; }
+        public string Message { get; set; }
+        public DateTime TimeStamp { get; set; }
+
+        public string TimeStampString => TimeStamp.ToString("dd-MM-yyyy, hh:mm:ss");
+    }
+}
