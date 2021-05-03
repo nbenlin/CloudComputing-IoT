44f8fa3eff19347b2011482e036f38a214a8a498
Nihat <nihatbenli.nb@gmail.com>
2021-05-03 17:22:33 +0200

    Add files via upload

---
+++ Mobile apps/Lab 3/PeopleDataStoreApp/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using PeopleDataStoreApp.DataContracts;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PeopleDataStoreApp
+{
+    public partial class App : Application
+    {
+        private const string API_URL = "http://192.168.1.66:5000/api";
+        public App()
+        {
+            InitializeComponent();
+
+            var client = RestEase.RestClient.For<IPeopleClient>(API_URL);
+
+            MainPage = new MainPage(client);
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
