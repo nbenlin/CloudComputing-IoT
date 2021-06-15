14d34ebe6e63032c3b38251d5b3cf43c6e6b172c
Nihat <nihatbenli.nb@gmail.com>
2021-06-15 15:36:19 +0200

    Add files via upload

---
+++ Mobile apps/Lab 8 Simple Xamarin Navigation/SimpleLocationApp/SimpleLocationApp/Localization.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+
+namespace SimpleLocationApp
+{
+    public static class Localization
+    {
+
+        public static async Task<string> EncodeLocation(this Location location)
+        {
+            try
+            {
+                var lat = location.Latitude;
+                var lon = location.Longitude;
+
+                var placemarks = await Geocoding.GetPlacemarksAsync(lat, lon);
+
+                var placemark = placemarks?.FirstOrDefault();
+                if (placemark != null)
+                {
+                    var geocodeAddress =
+                        $"AdminArea:       {placemark.AdminArea}\n" +
+                        $"CountryCode:     {placemark.CountryCode}\n" +
+                        $"CountryName:     {placemark.CountryName}\n" +
+                        $"FeatureName:     {placemark.FeatureName}\n" +
+                        $"Locality:        {placemark.Locality}\n" +
+                        $"PostalCode:      {placemark.PostalCode}\n" +
+                        $"SubAdminArea:    {placemark.SubAdminArea}\n" +
+                        $"SubLocality:     {placemark.SubLocality}\n" +
+                        $"SubThoroughfare: {placemark.SubThoroughfare}\n" +
+                        $"Thoroughfare:    {placemark.Thoroughfare}\n";
+
+                    return geocodeAddress;
+                }
+            }
+            catch (FeatureNotSupportedException fnsEx)
+            {
+                // Feature not supported on device
+            }
+            catch (Exception ex)
+            {
+                // Handle exception that may have occurred in geocoding
+            }
+            return null;
+
+        }
+
+        public static async Task<Location> GetMyLocation()
+        {
+            try
+            {
+                var location = await Geolocation.GetLastKnownLocationAsync();
+
+                if (location != null)
+                {
+                    Console.WriteLine($"Latitude: {location.Latitude}, Longitude: {location.Longitude}, Altitude: {location.Altitude}");
+                }
+
+                return location;
+            }
+            catch (FeatureNotSupportedException fnsEx)
+            {
+                // Handle not supported on device exception
+            }
+            catch (FeatureNotEnabledException fneEx)
+            {
+                // Handle not enabled on device exception
+            }
+            catch (PermissionException pEx)
+            {
+                // Handle permission exception
+            }
+            catch (Exception ex)
+            {
+                // Unable to get location
+            }
+            return null;
+        }
+    }
+}
---
+++ Mobile apps/Lab 8 Simple Xamarin Navigation/SimpleLocationApp/SimpleLocationApp/MainPage.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace SimpleLocationApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            BtnGetLocation.Clicked += BtnGetLocationOnClicked;
+            BtnNavigate.Clicked += BtnNavigateOnClicked;
+            BtnShareLocation.Clicked += BtnShareLocationOnClicked;
+        }
+
+        private async void BtnShareLocationOnClicked(object sender, EventArgs e)
+        {
+            var location = await Localization.GetMyLocation();
+            var encodedLocation = await location.EncodeLocation();
+
+            await Share.RequestAsync(new ShareTextRequest
+            {
+                Text = encodedLocation,
+                Title = "My current location"
+            });        
+        }
+    
+
+        private async void BtnNavigateOnClicked(object sender, EventArgs e)
+        {
+            var options = new MapLaunchOptions { NavigationMode = NavigationMode.Driving };
+ 
+            var lat = 52.4078059;
+            var lon = 16.9314785;
+
+            try
+            {
+                await Map.OpenAsync(new Location(lat, lon), options);
+            }
+            catch (Exception ex)
+            {
+                // No map application available to open
+            }
+        }
+
+        private async void BtnGetLocationOnClicked(object sender, EventArgs e)
+        {
+            var location = await Localization.GetMyLocation();
+            var encodedLocation = await location.EncodeLocation();
+        }
+    }
+}
